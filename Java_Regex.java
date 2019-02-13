e9ceb8c38afe24818da61812e3b643319a6aa684
Vakindu Philliam <Pvakindu@gmail.com>
2019-02-13 16:19:24 -0600

    Add files via upload

---
+++ Java_Regex.java
@@ -0,0 +1,31 @@
+package com.tutorialspoint;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LogicalOperatorDemo {
+   private static final String REGEX = "t|o";
+   private static final String INPUT = "Welcome to TutorialsPoint.COM";
+
+   public static void main(String[] args) {
+      // create a pattern
+      Pattern  pattern = Pattern.compile(REGEX);
+      
+      // get a matcher object
+      Matcher matcher = pattern.matcher(INPUT); 
+
+      while(matcher.find()) {
+         //Prints the start index of the match.
+         System.out.println("Match String start(): "+matcher.start());
+      }
+   }
+}
+
+//Results
+//Match String start(): 4
+//Match String start(): 8
+//Match String start(): 9
+//Match String start(): 13
+//Match String start(): 14
+//Match String start(): 21
+//Match String start(): 24
